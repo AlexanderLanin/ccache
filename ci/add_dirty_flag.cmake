fd83cbdf19b5121314ac0d03804780dc9d0b95e3
Alexander Lanin <alex@lanin.de>
2020-09-20 01:09:48 +0200

    let's try ..

--- ci/add_dirty_flag.cmake
+++ ci/add_dirty_flag.cmake
@@ -1,2 +1,2 @@
-include(cmake/CCacheVersion.cmake)
-file(WRITE cmake/CCacheVersion.cmake "set(CCACHE_VER ${CCACHE_VER}.dirty)\n")
+include(../cmake/CCacheVersion.cmake)
+file(WRITE ../cmake/CCacheVersion.cmake "set(CCACHE_VER ${CCACHE_VER}.dirty)\n")
