629237eaa6fc6624e35500b8a69d94af50fc8451
Alexander Lanin <alex@lanin.de>
2020-09-20 01:01:22 +0200

    add runs-on

---
+++ .cmake-format.py
@@ -0,0 +1,5 @@
+# This configuration should work with cmake-format 0.6.10 and higher.
+with section("format"):
+  # allow set(VAR VALUE PARENT_SCOPE) to remain on one line
+  max_subgroups_hwrap = 3
+  #max_pargs_hwrap = 60
